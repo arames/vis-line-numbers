fafa9f8b73ec6f3dcc873cef070cfc43277aad0d
Alexandre Rames <alexandre.rames@uop.re>
2017-04-05 08:58:29 -0700

    Update for the new associated vis pull request.
    See https://github.com/martanne/vis/pull/525.

--- vis-line-numbers.lua
+++ vis-line-numbers.lua
@@ -3,20 +3,23 @@ require('vis')
 local module = {}
 
 vis.events.subscribe(vis.events.MODE_CHANGE, function()
-  if (vis.mode == vis.modes.INSERT) then
-    vis:command('set numbers')
-  else
-    vis:command('set relativenumbers')
-  end
+	if (vis.mode == vis.modes.INSERT) then
+		vis:command('set numbers')
+	else
+		vis:command('set relativenumbers')
+	end
 end)
 
-vis.events.subscribe(vis.events.WIN_FOCUS, function(focused_win)
-  for win in vis:windows() do
-    if win ~= focused_win then
-      vis:command('set numbers', win)
-    end
-  end
-  vis:command('set relativenumbers', focused_win)
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command('set numbers', win)
+end)
+
+vis.events.subscribe(vis.events.WIN_LEAVE, function(win)
+	vis:command('set numbers')
+end)
+
+vis.events.subscribe(vis.events.WIN_ENTER, function(win)
+	vis:command('set relativenumbers')
 end)
 
 return module
